c58d6fda9d1996afa9b2269fbb9fe6f7fce8baf9
HigorLuizz <higor.luiz1997@gmail.com>
2022-07-14 17:46:11 -0300

    Exercício 04

---
+++ Exercício 04/Readme.md
@@ -0,0 +1,3 @@
+M1S03 Ex 04 - DOM
+Crie um site (arquivo HTML) que, ao ser renderizado, adicione (arquivo JS)
+5 textos quaisquer, um ao lado outro, com a cor azul (arquivo css).
---
+++ Exercício 04/exercício04.js
@@ -0,0 +1,15 @@
+const txt1 = document.getElementsByClassName('txt1')[0]
+txt1.innerHTML = "Texto 01 de teste"
+
+const txt2 = document.getElementsByClassName('txt2')[0]
+txt2.innerHTML = "Texto 02 de teste"
+
+const txt3 = document.getElementsByClassName('txt3')[0]
+txt3.innerHTML = "Texto 03 de teste"
+
+const txt4 = document.getElementsByClassName('txt4')[0]
+txt4.innerHTML = "Texto 04 de teste"
+
+const txt5 = document.getElementsByClassName('txt5')[0]
+txt5.innerHTML = "Texto 05 de teste"
+
